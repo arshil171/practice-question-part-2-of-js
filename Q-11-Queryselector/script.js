fcf77d00c2a5d3ef167cd1e1ddba92243b151a28
arshil171 <ARSHILGONDALIYA5@GMAIL.COM>
2025-04-11 16:01:00 +0530

    version1

---
+++ Q-11-Queryselector/script.js
@@ -0,0 +1,13 @@
+// Q - 1 : Answer
+
+
+let heading = document.querySelector("#heading")
+
+heading.innerHTML = " Q - 1 :- Welcome to the JavaScript World"
+
+
+// Q - 2 : Answer
+
+let updated = document.querySelectorAll("p")
+
+updated.innerHTML = "Updated Content"
